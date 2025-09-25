8fde4565013526fd2cd0b5de23af392fd711ab90
CamilaMelara <20235691@esen.edu.sv>
2025-09-25 11:38:59 -0600

    uploading first commits

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,17 @@
+import 'package:go_router/go_router.dart';
+import 'package:link_up/feed/event_feed_screen.dart';
+import 'package:link_up/login/login_screen.dart';
+import 'package:link_up/register/register_screen.dart';
+import 'package:link_up/welcome/welcome_screen.dart';
+
+
+final GoRouter appRouter = GoRouter(
+  initialLocation: '/welcome',
+  routes: [
+      GoRoute(path: '/welcome', builder: (context, state) => const WelcomeScreen()),
+      GoRoute(path: '/login', builder: (context, state) => const LoginScreen()),
+      GoRoute(path: '/register', builder: (context, state) => const RegisterScreen()),
+      GoRoute(path: '/feed', builder: (context, state) => const EventFeedScreen()),
+
+  ],
+);
